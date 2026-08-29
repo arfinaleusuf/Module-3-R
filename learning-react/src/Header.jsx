a424296e7825b5b623dc8a189f39a65c37557e58
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-30 02:34:42 +0600

    Refactor code structure for improved readability and maintainability

--- learning-react/src/Header.jsx
+++ learning-react/src/Header.jsx
@@ -1,17 +1,31 @@
 
-import { useContext } from 'react';
-import AuthButton from './AuthButton';
-import { AuthContext } from './context/AuthProvider';
-
+// import { useContext } from 'react';
+// import AuthButton from './AuthButton';
+// import { AuthContext } from './context/AuthProvider';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
-
-    const {authUser} = useContext(AuthContext)
+    // const {authUser} = useContext(AuthContext)
 
     return (
-        <div>
-            <p>From Header, Name is: {authUser?.name} And id is {authUser?.id}</p>
-            <AuthButton/>
+        <div className="flex justify-between bg-amber-500 py-3 px-6 text-white">
+            {/* <p>From Header, Name is: {authUser?.name} And id is {authUser?.id}</p>
+            <AuthButton/> */}
+            <p>Our Web</p>
+            <div className="hidden lg:block">
+                <ul className="flex gap-5">
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+
+            <div>
+                <button className="btn hidden lg:block">Login</button>
+                <GiHamburgerMenu className="block lg:hidden"/>
+
+            </div>
+
         </div>
     );
 };
