1bfa7cc0c021953b00d65970cad243943ac1004d
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-30 01:24:02 +0600

    Add user information display to Header component

--- learning-react/src/Header.jsx
+++ learning-react/src/Header.jsx
@@ -1,9 +1,16 @@
 
+import { useContext } from 'react';
 import AuthButton from './AuthButton';
+import { AuthContext } from './context/AuthProvider';
+
 
 const Header = () => {
+
+    const {authUser} = useContext(AuthContext)
+
     return (
         <div>
+            <p>From Header, Name is: {authUser?.name} And id is {authUser?.id}</p>
             <AuthButton/>
         </div>
     );
