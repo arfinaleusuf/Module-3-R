37d1940bf4732e48f098c8b8dfece906b22f71eb
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-30 02:02:45 +0600

    basic responsive

--- learning-react/src/Layout.jsx
+++ learning-react/src/Layout.jsx
@@ -6,9 +6,9 @@ const Layout = () => {
         <div>
             {/* <Header /> */}
             <div className="grid grid-cols-3 grid-rows-2 gap-2">
-                <div className="row-span-2 p-12 bg-amber-400  text-center">1</div>
-                <div className="col-span-2 p-12 bg-red-400  text-center">2</div>
-                <div className="col-span-2 p-12 bg-green-400  text-center">3</div>
+                <div className="p-12 bg-amber-400 sm:bg-cyan-400 md:bg-red-400 lg:bg-yellow-400 text-center">1</div>
+                <div className="p-12 bg-red-400  text-center">2</div>
+                <div className="p-12 bg-green-400  text-center">3</div>
             </div>
 
             {/* <ul className="flex gap-12">
