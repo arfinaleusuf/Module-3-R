2074f316b54b634679c2ed8c1f96fcd73800e6d3
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-30 01:54:35 +0600

    refactor: update Layout component structure and styling

--- learning-react/src/Layout.jsx
+++ learning-react/src/Layout.jsx
@@ -4,8 +4,18 @@ import Header from "./Header";
 const Layout = () => {
     return (
         <div>
-            <Header />
-            <h1 className="text-red-500 font-black text-4xl mt-12">Our content</h1>
+            {/* <Header /> */}
+            <div className="grid grid-cols-3 grid-rows-2 gap-2">
+                <div className="row-span-2 p-12 bg-amber-400  text-center">1</div>
+                <div className="col-span-2 p-12 bg-red-400  text-center">2</div>
+                <div className="col-span-2 p-12 bg-green-400  text-center">3</div>
+            </div>
+
+            {/* <ul className="flex gap-12">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contacts</li>
+            </ul> */}
         </div>
     );
 };
